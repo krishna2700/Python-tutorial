c89245cae09eddcee7372857fe6af57103dc3dba
krishna2700 <krishnaruparelia0207@gmail.com>
2025-05-03 10:26:11 +0530

    lists , tuples and sets

---
+++ lists_tuples_sets/code.py
@@ -0,0 +1,39 @@
+l = ["cats", "dogs", "mice"]
+t = ("cats", "dogs", "mice")
+s = {"cats", "dogs", "mice"}
+
+# l[0]= "fish"
+# print(l)
+
+# t[0]= "fish"
+# print(t)
+
+# s[0]= "fish"
+# print(s)
+
+# l.append("fish")
+# print(l)
+
+# t.append("fish")
+# print(t)
+
+# s.append("fish")
+# print(s)
+
+# l.remove("cats")
+# print(l)
+
+# s.remove("cats")
+# print(s)
+
+# t.remove("cats")
+# print(t)
+
+# l.add("fish")
+# print(l)
+
+# t.add("fish")
+# print(t)
+
+# s.add("fish")
+# print(s)
