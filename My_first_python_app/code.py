c9cf5eeec55ebb64515e7c3588233cc36b6b5688
krishna2700 <krishnaruparelia0207@gmail.com>
2025-05-03 10:05:32 +0530

    small app practice python

---
+++ My_first_python_app/code.py
@@ -0,0 +1,4 @@
+user_age = int(input("Enter your age: "))
+months = user_age * 12
+seconds = user_age * 365 * 24 * 60 * 60
+print(f"You are {user_age} years old, which is {months} months old. That makes you {seconds} seconds old.")
