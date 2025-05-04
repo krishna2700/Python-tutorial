7fd47deaa15bc77904488b74e03586faa88a9c9e
krishna2700 <krishnaruparelia0207@gmail.com>
2025-05-04 08:36:28 +0530

    booleans

---
+++ booleans/code.py
@@ -0,0 +1,15 @@
+# print (5 == 5)
+# print (5 > 5)
+# print (10 != 10)
+
+
+# friends = {"Bob", "Alice", "Charlie", "John"}
+# abroad = {"Bob", "Alice", "David","Eve"}
+
+friends = ["Bob", "Alice",]
+abroad = ["Bob", "Alice", ]
+
+print (friends == abroad)
+
+# print (friends is abroad)
+# print (friends is friends)
