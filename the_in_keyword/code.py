84dc202f2e246c5e31399fee3bd4a80bd728014a
krishna2700 <krishnaruparelia0207@gmail.com>
2025-05-18 11:40:50 +0530

    in statemnet

---
+++ the_in_keyword/code.py
@@ -0,0 +1,15 @@
+friends = ["Alice", "Bob", "Charlie",]
+
+# print("jenny" in friends)  # False
+# print("Alice" in friends)  # True
+
+
+movies = ["Inception", "The Matrix", "Interstellar"]
+
+# print("Inception" in movies)  # True
+# print("Interstellar" in movies)  # True
+# print("Avatar" in movies)  # False
+
+user_movies = input("Enter something you like: ")
+
+print(user_movies in movies)
