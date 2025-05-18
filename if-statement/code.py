cd510beeaee224f168c1057e50fe72c9f726f5c8
krishna2700 <krishnaruparelia0207@gmail.com>
2025-05-18 11:23:05 +0530

    if statemnet

---
+++ if-statement/code.py
@@ -0,0 +1,10 @@
+day_of_week = input("What day of the week is it today?").lower()
+
+if day_of_week == "Monday":
+    print("Have a great start to the week!")
+elif day_of_week == "Friday":
+    print("It's almost the weekend!")
+else:
+    print("Full speed ahead!")    
+
+# print("This always runs, regardless of the day of the week.")
