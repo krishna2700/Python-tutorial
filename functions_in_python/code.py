646b3ab3f7370c33fb0c15db916590fc118e60fa
krishna2700 <krishnaruparelia0207@gmail.com>
2025-06-08 09:00:23 +0530

    functions in python

---
+++ functions_in_python/code.py
@@ -0,0 +1,39 @@
+# def hello ():
+#     print("Hello, World!")
+
+
+# hello()
+
+# def user_age_in_seconds():
+#     user_age = int(input("Enter your age: "))
+#     age_seconds = user_age * 365 * 24 * 60 * 60
+#     print(f"You are {age_seconds} seconds old.")
+
+
+
+# user_age_in_seconds()
+
+
+# def print():
+#     print("Hello, World!")
+
+# print()    
+
+# friends = ["Alice", "Bob", "Charlie", "David"]
+
+# def add_friend():
+#     friend_nmae= input("Enter the name of your friend: ")
+#     friends = friends + [friend_nmae]
+#     print(friends)
+
+# add_friend()    
+
+friends = [] 
+def add_friend():
+    friends.append("Jenny")
+    
+add_friend()
+add_friend()
+add_friend()
+
+print(friends)  # Output: ['Jenny']
