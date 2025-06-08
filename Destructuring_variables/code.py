39d67eaf0122e68dbbd8c939b963f62ce536aea8
krishna2700 <krishnaruparelia0207@gmail.com>
2025-06-08 08:41:09 +0530

    destructuring variabled

---
+++ Destructuring_variables/code.py
@@ -0,0 +1,33 @@
+# t= 5 , 11
+
+# x, y = t
+# print(x, y)
+# print(x)
+# print(y)
+
+# student_attendance = {"Rolf": 96,
+#                       "Bob": 80,
+#                       "Anne": 100,
+#                       "Charlie": 90}
+
+# print(list(student_attendance.items()))  # List of student names
+
+# for student, attendance in student_attendance.items():
+#     print(f"{student} has {attendance}% attendance")
+
+# people =[("Bob", 20, "Mechanic"),
+#          ("Alice", 22, "Engineer"),
+#          ("Charlie", 19, "Designer"),
+#          ("David", 21, "Designer")]
+
+# for name, age, profession in people:
+#     print(f"{name} is {age} years old and works as a {profession}.")
+
+# person = ("Bob", 20, "Mechanic")
+
+# print(f"{person[0]} is {person[1]} years old and works as a {person[2]}.")
+
+head , *tail = [1,2,3,4,5]
+
+print(head)  # Output: 1
+print(tail)  # Output: [2, 3, 4, 5]
