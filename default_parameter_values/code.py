756b7af2e6f16853b19cc7e2a697ce5876e4e0b5
krishna2700 <krishnaruparelia0207@gmail.com>
2025-06-21 21:31:22 +0530

    default parameter value

---
+++ default_parameter_values/code.py
@@ -0,0 +1,5 @@
+def add(x,y=8):
+    print(f"The sum of {x} and {y} is {x + y}")
+
+add(10,20)
+add(10)  # Uses default value for y
--- the_in_keyword/code.py
+++ the_in_keyword/code.py
@@ -8,6 +8,8 @@ movies = ["Inception", "The Matrix", "Interstellar"]
 
 # print("Inception" in movies)  # True
 # print("Interstellar" in movies)  # True
+
+
 # print("Avatar" in movies)  # False
 
 user_movies = input("Enter something you like: ")
