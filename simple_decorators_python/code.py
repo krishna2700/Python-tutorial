1812a10af492fff6ac2e965f5b3012332c926377
krishna2700 <krishnaruparelia0207@gmail.com>
2025-07-12 21:56:36 +0530

    simple decorators

---
+++ simple_decorators_python/code.py
@@ -0,0 +1,27 @@
+user ={"username":"jose","access_level":"guest"}
+
+def get_admin_password():
+    return "1234"
+
+# def secure_get_admin():
+#     if user["access_level"] == "admin":
+#         return "1234"
+    
+
+# print(secure_get_admin())  # This will not print anything since user is a guest.    
+
+def make_secure(func):
+    def secure_get_admin():
+        if user["access_level"] == "admin":
+            return func()
+        else:
+            return f"Access denied: {user['username']} does not have admin privileges."
+            # raise PermissionError("You do not have permission to access this resource.")
+        
+    return secure_get_admin
+   
+    
+get_admin_password = make_secure(get_admin_password)    
+
+
+print(get_admin_password())  # This prints the admin password, which is "1234".
